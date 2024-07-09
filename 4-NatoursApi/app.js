7c0ce2569acadc009e0d4e732dbfad73a7337d1c
Tony <tonytnportela@outlook.com>
2024-07-09 18:28:48 -0300

    downloaded the material for this part of the course

--- 4-NatoursApi/app.js
+++ 4-NatoursApi/app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(202);
-  res.json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+//app.get('/', (req, res) => {
+//  res.status(202);
+//  res.json({ message: 'Hello from the server side!', app: 'Natours' });
+//});
+//
+//app.post('/', (req, res) => {
+//  res.send('You can post to this endpoint')
+//})
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint')
-})
+app.get('/api/v1/tours', (req,res) => {
+  
+}) 
 
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
