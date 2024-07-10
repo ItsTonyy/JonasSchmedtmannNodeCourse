befa803e0a3cd867a50013f0d6bb6b9be27b3182
Tony <tonytnportela@outlook.com>
2024-07-09 23:06:12 -0300

    added routes get and post

--- 4-NatoursApi/app.js
+++ 4-NatoursApi/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 //app.get('/', (req, res) => {
 //  res.status(202);
@@ -12,9 +15,35 @@ const port = 3000;
 //  res.send('You can post to this endpoint')
 //})
 
-app.get('/api/v1/tours', (req,res) => {
-  
-}) 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200);
+  res.json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201)
+    res.json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+  })
+});
 
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
