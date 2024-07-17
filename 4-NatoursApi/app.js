09ad5d2bb4348d395fa05356ccc51f5bb666b01a
Tony <tonytnportela@outlook.com>
2024-07-17 15:45:37 -0300

    handling errors in app.js + added git ignore to the repository

--- 4-NatoursApi/app.js
+++ 4-NatoursApi/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -9,4 +12,11 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports = app
+// error handling in the routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server.`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
